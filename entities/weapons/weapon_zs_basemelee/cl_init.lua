51cef00a0cbbece00ceeefce5bd3715176ea677f
Toy323 <83911582+Toy323@users.noreply.github.com>
2022-05-28 13:14:57 +0900

    Better block vision

--- entities/weapons/weapon_zs_basemelee/cl_init.lua
+++ entities/weapons/weapon_zs_basemelee/cl_init.lua
@@ -23,17 +23,26 @@ end
 
 function SWEP:DrawHUD()
 	if GetConVar("crosshair"):GetInt() ~= 1 then return end
-	if self.BlockTrue == true then
+	if self.BlockTrue == true and self:GetOwner():GetInfo("zs_blockunable") == "0" then
 	self:DrawCrosshairDot()
-	local wid, hei = 384, 16
-	local x, y = ScrW() - wid - 32, ScrH() - hei - 72
+	local wid, hei = 500, 900
+	local x, y = ScrW() - wid - 602, ScrH() - hei - 72
 	local texty = y - 4 - draw.GetFontHeight("ZSHUDFontSmall")
 	if self.Block == 1 then
 	draw.SimpleText(""..translate.Get("blockistrue"), "ZSHUDFontSmall", x + wid, texty, COLOR_GREEN, TEXT_ALIGN_RIGHT)
 	else
 		draw.SimpleText(""..translate.Get("blockisfalse"), "ZSHUDFontSmall", x + wid, texty, COLOR_RED, TEXT_ALIGN_RIGHT)
 	end
-end
+elseif self:GetOwner():GetInfo("zs_blockunable") == "1" then
+	local wid1, hei1 = 384, 16
+	local x1, y1 = ScrW() - wid1 - 32, ScrH() - hei1 - 72
+	local texty1 = y1 - 4 - draw.GetFontHeight("ZSHUDFontSmall")
+	if self.Block == 1 then
+	draw.SimpleText(""..translate.Get("blockistrue"), "ZSHUDFontSmall", x1 + wid1, texty1, COLOR_GREEN, TEXT_ALIGN_RIGHT)
+	else
+		draw.SimpleText(""..translate.Get("blockisfalse"), "ZSHUDFontSmall", x1 + wid1, texty1, COLOR_RED, TEXT_ALIGN_RIGHT)
+	end
+end 
 end
 
 function SWEP:OnRemove()
