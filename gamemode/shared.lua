4d7e15b306f09968b6d6dab1694e633110c08331
Toy323 <83911582+Toy323@users.noreply.github.com>
2022-09-07 16:45:14 +0900

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -5,7 +5,7 @@ GM.Website	=	"https://github.com/erioxis/dozet"
 
 -- No, adding a gun doesn't make your name worth being here.
 GM.Credits = {
-	{"Version", "Legendary", "3.7.3"},
+	{"Version", "Legendary", "3.7.7"},
 	{"erioxis", "Phantom coder", "dead"},
 	{"Nullted", "", "RU-ENG Translation"}
 
